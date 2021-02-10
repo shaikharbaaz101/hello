b796e4c07b1dbb0305c834870ad9b4cf68bb2568
Dhanashree.Karambalkar <dhanashree.karambalkar@atcs.com>
2021-02-10 08:08:08 +0530

    changes done

--- src/main/java/com/example/demo/api/EmpApi.java
+++ src/main/java/com/example/demo/api/EmpApi.java
@@ -3,11 +3,12 @@ package com.example.demo.api;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
@@ -54,5 +55,20 @@ public class EmpApi {
 		return employeesRepo.add(e);
 		
 	}
+	/*
+	 * @PutMapping("/update/{id}") public Employee updateEmp(@RequestBody Employee
+	 * e) { int x= e.getId(); if (x==0) { System.out.println("not an entry"); } else
+	 * { return employeesRepo.get(e.setName("sohail"); }
+	 */
+	
+	@DeleteMapping("/delete/{id}")
+		public Employee deleteEmployeeById(@PathVariable int id) {
+			
+		
+		return employeesRepo.remove(id);
+		}
+		
+	
+	}
 
-}
+
